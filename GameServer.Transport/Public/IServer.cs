88ef86257777f06d9133801a1a37b806eef40490
Niyaz Kashafutdinov <niyaz.kashafutdinov@colossi.games>
2023-10-19 14:16:59 +0700

    merged

--- GameServer.Transport/Public/IServer.cs
+++
@@ -1,9 +0,0 @@
-namespace Remouse.GameServer.ServerTransport
-{
-    public interface IServer
-    {
-        public void Start(int port, int maxConnections);
-        public void Stop();
-        public void Update();
-    }
-}
